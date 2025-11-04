c8ec80171275c03ff7a5c2a8b19a1e3ccb4ad16c
ManuelPerezF <A01741957@tec.mx>
2025-11-04 15:42:00 -0600

    si

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(message="El tec deberia dar mas becas")
+    return jsonify(message="El tec deberia dar mas becas, porfa")
 
 
 if __name__ == '__main__':
